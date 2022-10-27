a31cd589bb903c838acfb1f92adebeddb4536c4c
Naren Sulegai <86naren@gmail.com>
2022-10-27 15:33:50 -0700

    Use describe block

--- src/components/Cell.test.js
+++ src/components/Cell.test.js
@@ -1,11 +1,13 @@
 import {render, screen, fireEvent} from '@testing-library/react';
 import Cell from './Cell';
 
-test('should callback with row and col when clicked', (done) => {
-    render(<Cell row={0} col={1} onClick={(row, col) => {
-        expect(row).toBe(0)
-        expect(col).toBe(1)
-        done()
-    }}/>);
-    fireEvent.click(screen.getByRole('button'))
-});
+describe('Cell', () => {
+    it('should callback with row and col when clicked', (done) => {
+        render(<Cell row={0} col={1} onClick={(row, col) => {
+            expect(row).toBe(0)
+            expect(col).toBe(1)
+            done()
+        }}/>);
+        fireEvent.click(screen.getByRole('button'))
+    });
+})
